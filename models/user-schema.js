5dab30e40382dee02fb8f7aafe0e45d4026ca722
Jaime Pagan <jimbopagan@yahoo.com>
2017-05-06 18:04:47 -0400

    added models

---
+++ models/user-schema.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/, 'Please fill a valid email address']
+    },
+    phone: Number,
+    launchTime: {
+        description: String,
+        time: function () {
+            return new Date(); // current date and time
+        }
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    },
+    userHoles: [{
+        type: Schema.Types.ObjectId,
+        ref: "Hole"
+    }]
+})
+
+module.exports = mongoose.model('User', userSchema);
