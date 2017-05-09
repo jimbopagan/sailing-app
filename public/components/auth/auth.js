b80750615d9f4bdc39f158ab9323ebb77e76b556
Ayanda Matiringe <amatirin92@gmail.com>
2017-05-09 15:11:24 -0600

    updated index

--- public/components/auth/auth.js
+++ public/components/auth/auth.js
@@ -7,7 +7,7 @@ app.config(["$routeProvider", function ($routeProvider) {
     $routeProvider
         .when('/home', {
             templateUrl: "components/home/home.html",
-            controller: 'HomeController',
+            controller: 'HomeController'
         })
         .when("/profile", {
             templateUrl: "components/profile/profile.html",
